9c972816721e3fdbcf7266f6750f92a062b61727
andigvozdjar <andigvozdjar@gmail.com>
2020-11-18 23:44:21 +0100

    aa

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { Text, StyleSheet, View, Image, Button, FlatList } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
 const color_increment = 20;
 
-const SquareScreen = (prop) => {
-    const [ red, setRed ] = useState(0);
-    const [ green, setGreen ] = useState(0);
-    const [ blue, setBlue ] = useState(0);
+const reducer = (state, action) => {
+    
+}
+const SquareScreen = () => {
 
-    console.log(`rgb(${red}, ${green}, ${blue})`);
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
 
     return (
         <View>
-            <ColorCounter onIncrease={() => setRed(red + color_increment)} onDecrease={() => setRed(red - color_increment)} color="Red"/>
-            <ColorCounter onIncrease={() => setBlue(blue + color_increment)} onDecrease={() => setBlue(blue - color_increment)} color="Blue"/>
-            <ColorCounter onIncrease={() => setGreen(green + color_increment)} onDecrease={() => setGreen(green - color_increment)} color="Green"/>
+            <ColorCounter onIncrease={() => } onDecrease={() =>  } color="Red"/>
+            <ColorCounter onIncrease={() => } onDecrease={() =>  } color="Blue"/>
+            <ColorCounter onIncrease={() => } onDecrease={() =>  } color="Green"/>
             <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }}/>
         </View>
     )
